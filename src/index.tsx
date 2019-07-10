9e68025090fd72b154d041bdb338034149525157
Rom <rdewolff@users.noreply.github.com>
2019-07-10 11:16:33 +0200

    Checkpoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,26 @@
-import * as React from "react";
-import { render } from "react-dom";
-import { observer } from "mobx-react";
-import { types, getRoot } from "mobx-state-tree";
+import * as React from 'react'
+import { render } from 'react-dom'
+import { observer } from 'mobx-react'
+import { types, getRoot } from 'mobx-state-tree'
 
-import "./styles.css";
+import './styles.css'
 
 @observer
 class App extends React.Component<
   {
-    store: any;
+    store: any
   },
   {}
 > {
-  store: any;
+  store: any
 
   constructor(props) {
-    super(props);
-    this.store = props.store;
+    super(props)
+    this.store = props.store
   }
   render() {
     // const { store } = props
-    const store = this.store;
+    const store = this.store
     return (
       <div className="App">
         <h1>Mobx State Tree</h1>
@@ -39,35 +39,35 @@ class App extends React.Component<
           lazy load
         </button>
       </div>
-    );
+    )
   }
 }
 
 function User(props) {
-  return <div>{props.user.name}</div>;
+  return <div key={props.id}>{props.user.name}</div>
 }
 
 const Business = types
   .model({
     _id: types.identifier,
-    name: "",
+    name: '',
     owner: types.array(types.late(() => LazyPeople))
   })
   .actions(self => ({
     addPerson() {
-      console.log("Business.addPerson()");
+      console.log('Business.addPerson()')
       // add a random person
-      self.owner.push((Math.floor(Math.random() * 10) + 1).toString());
-      console.log(JSON.stringify(self.owner));
+      self.owner.push((Math.floor(Math.random() * 10) + 1).toString())
+      console.log(JSON.stringify(self.owner))
     }
-  }));
+  }))
 
 // https://swapi.co/api/people/
 const People = types.model({
   _id: types.identifier,
-  name: ""
+  name: ''
   // favorites: types.array(types.reference(types.late(() => Business)))
-});
+})
 
 // caching mechanism
 const CachePeople = types.model().actions(self => {
@@ -75,43 +75,43 @@ const CachePeople = types.model().actions(self => {
   // id :
   // date :
   // value :
-  let cache = {};
-  const duration = 10000; // ms
+  let cache = {}
+  const duration = 10000 // ms
   const save = identifier => {
     cache[identifier] = {
       value: getPeople(identifier),
       date: new Date()
-    };
-  };
+    }
+  }
   const get = (identifier: string) => {
     // check cache entry
     if (cache[identifier]) {
-      const cached = cache[identifier];
+      const cached = cache[identifier]
       if (new Date() - cached.date < duration) {
-        console.log("from cache");
-        return cached.value;
+        console.log('from cache')
+        return cached.value
       }
     }
     // otherwise we load it and cache it
-    console.log("load");
-    self.save(identifier);
+    console.log('load')
+    self.save(identifier)
 
-    return cache[identifier].value;
-  };
+    return cache[identifier].value
+  }
 
   return {
     get,
     save
-  };
-});
+  }
+})
 
 function getPeople(id) {
-  const url = "https://swapi.co/api/people/" + id + "/";
+  const url = 'https://swapi.co/api/people/' + id + '/'
   // const url = "http://localhost:3000/employees/" + id + "/";
-  var req = new XMLHttpRequest();
-  req.open("GET", url, false);
-  req.send(null);
-  return JSON.parse(req.responseText) || null;
+  var req = new XMLHttpRequest()
+  req.open('GET', url, false)
+  req.send(null)
+  return JSON.parse(req.responseText) || null
 }
 
 const LazyPeople = types.maybeNull(
@@ -122,7 +122,7 @@ const LazyPeople = types.maybeNull(
       // dynamic data loading - works 🎉
       // return getPeople(identifier);
       // dynamic data loading with cache - WIP
-      return getRoot(parent).cache.get(identifier);
+      return getRoot(parent).cache.get(identifier)
       // static data loading - works 🎉
       // return {
       //   _id: "1",
@@ -130,23 +130,23 @@ const LazyPeople = types.maybeNull(
       // };
     },
     set(value /* user */) {
-      return value ? value._id : null;
+      return value ? value._id : null
     }
   })
-);
+)
 
 const RootStore = types.model({
   businesses: types.array(Business),
   people: types.array(People),
   cache: CachePeople
-});
+})
 
 const rootStore = RootStore.create({
-  businesses: [{ _id: "b-1", name: "Meetme", owner: ["1", "2"] }],
+  businesses: [{ _id: 'b-1', name: 'Meetme', owner: ['1', '2'] }],
   people: [], // we want this to lazy load when required
   cache: {}
   // users: [{ _id: "100", phone: "555-1234", favorites: ["1", "2"] }]
-});
+})
 
-const rootElement = document.getElementById("root");
-render(<App store={rootStore} />, rootElement);
+const rootElement = document.getElementById('root')
+render(<App store={rootStore} />, rootElement)
